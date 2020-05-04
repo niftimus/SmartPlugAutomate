4bdb642d7db8e7419e9612f82e72efa6bb7b51ca
David Hamilton <david.hamilton@iinet.net.au>
2020-05-05 00:54:04 +1000

    Updated with plug control logic

--- smartcontrol.py
+++ smartcontrol.py
@@ -12,24 +12,61 @@ click.anyio_backend = "asyncio"
 @click.option('--plug_address', default="10.1.2.12", help='IP address of Smart Plug device.')
 @click.option('--solar_monitor_url', default="http://10.1.2.3/production.json", help='IP address of Solar Monitor device.')
 @click.option('--check_interval', default=1, help='Check interval in seconds.')
-@click.option('--min_power', default=400, help='Minimum solar power in Watts.')
-@click.option('--min_off', default=300, help='Minimum off period in seconds.')
-@click.option('--min_on', default=60, help='Minimum on period in seconds.')
+@click.option('--min_power', default=-370, help='Minimum solar power in Watts before switching on.')
+@click.option('--min_off', default=5, help='Minimum off period in seconds.')
+@click.option('--min_on', default=5, help='Minimum on period in seconds.')
 @click.pass_context
 async def main(ctx, plug_address, solar_monitor_url, check_interval, min_power, min_off, min_on):
     """Main monitoring loop"""
     plug = SmartPlug(plug_address)
+    last_ontime = time.time()
+    last_offtime = last_ontime
+    switchcount = 0
     while True:
+        action_string = ""
         await plug.update()
-        plugStatus = await plug.get_emeter_realtime()
-        print(plugStatus)
-        time.sleep(check_interval - (time.time() % check_interval))
+        plugRealtime = await plug.get_emeter_realtime()
+        is_on = plug.is_on
         r = requests.get(solar_monitor_url)
         solar_json = r.json()
-        current_time = time.ctime()
-        current_production = (solar_json["production"][1]["wNow"])
-        current_consumption = (solar_json["consumption"][0]["wNow"])
-        current_net = current_production - current_consumption
+        current_time = time.time()
+        overall_production = (solar_json["production"][1]["wNow"])
+        overall_consumption = (solar_json["consumption"][0]["wNow"])
+        overall_net = overall_production - overall_consumption
+        plug_consumption = plugRealtime["power_mw"]/1000
+
+        time_since_off = current_time - last_offtime
+        time_since_on = current_time - last_ontime
+
+        if(is_on):
+            if((overall_net + plug_consumption)>=min_power):
+                threshold_string = "Overall is above minimum."
+                action_string = "Leaving on."
+            else:
+                threshold_string = "Overall is under minimum."
+                if (time_since_on<min_on):
+                    action_string = "Leaving on."
+                else:
+                    action_string = "Turning off."
+                    await plug.turn_off()
+                    last_offtime = current_time
+                    switchcount += 1
+        else:
+            if((overall_net + plug_consumption)>=min_power):
+                threshold_string = "Overall is above minimum."
+                if (time_since_off<min_off):
+                    action_string = "Leaving off."
+                else:
+                    action_string = "Turning on."
+                    last_ontime = current_time
+                    await plug.turn_on()
+                    switchcount+=1
+            else:
+                threshold_string = "Overall is under minimum."
+                action_string = "Leaving off."
+
+        print(f'[{time.time()}] Overall W: {overall_net}, Plug W: {plug_consumption}, Secs since on: {int(time_since_on)}, Secs since off: {int(time_since_off)}, Switch count: {switchcount}, Last Plug State: {is_on} ==> {threshold_string} {action_string}')
+        time.sleep(check_interval - (time.time() % check_interval))
 
 
 if __name__ == "__main__":
